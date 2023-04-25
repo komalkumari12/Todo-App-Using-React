9ee79976fc6ae562b536ad7f872f0bd3f36e68d6
komal <kk6612465@gmail.com>
2023-04-25 23:25:46 +0530

    React FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [Task, setTask] = useState("");
+  const [Description, setDescription] = useState("");
+  const [todos, setTodo] = useState([]);
+  console.log("Hello");
+
+  const SubmitTask = function () {
+    if (Task === "" || Description === "") {
+      alert("Empty Fields");
+      return;
+    }
+
+    //Add to Database
+    const newTodo = {
+      id: todos.length + 1,
+      Task,
+      Description,
+    };
+    setTodo([...todos, newTodo]);
+    setTask("");
+    setDescription("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>Title</label>
+        <input
+          type="text"
+          placeholder="Todo Task"
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
+        ></input>
+        <label>Description</label>
+        <input
+          type="text"
+          placeholder="Description"
+          onChange={(event) => {
+            setDescription(event.target.value);
+          }}
+        ></input>
+
+        <button type="submit" onSubmit={SubmitTask}>
+          Submit
+        </button>
+      </form>
+      <ul>
+        {todos.map((App, id) => {
+          <li key={App.id}>
+            <p>{App.Task}</p>
+            <p>{App.Description}</p>
+          </li>;
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
