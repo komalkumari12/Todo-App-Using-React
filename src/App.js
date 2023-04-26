a7f2379c0474a950d96a14888807fc1013ab64a6
komal <kk6612465@gmail.com>
2023-04-26 20:39:12 +0530

    connected frontend with backend through axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 const App = () => {
-  const [Task, setTask] = useState("");
-  const [Detail, setDetail] = useState("");
+  const [title, setTask] = useState("");
+  const [description, setDetail] = useState("");
   const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/getAllTodos").then((res) => {
+      console.log(res.data.message);
+      setTodos(res.data.message);
+    });
+  }, []);
 
-  function SubmitTask(event) {
-    event.preventDefault();
+  async function SubmitTask(event) {
+    try {
+      event.preventDefault();
 
-    if (Task === "" || Detail === "") {
-      alert("Fill Required Details");
-      return;
+      if (title === "" || description === "") {
+        alert("Fill Required Details");
+        return;
+      }
+
+      const response = await axios.post("http://localhost:5000/api/addTodo", {
+        title: title,
+        description: description,
+      });
+
+      console.log(response);
+      setTodos([...todos, response.data.message]);
+      setTask("");
+      setDetail("");
+    } catch (error) {
+      if (error && error.response) {
+        console.log(error.response.message);
+        return;
+      }
+      console.log(error.message);
     }
-
-    const newtodos = {
-      id: todos.length + 1,
-      Task,
-      Detail,
-    };
-
-    setTodos([...todos, newtodos]);
-    setTask("");
-    setDetail("");
   }
 
-  function deleteTodo(id) {
-    const newTodos = todos.filter((todo) => todo.id !== id);
+  async function deleteTodo(id) {
+    const newTodos = todos.filter((todo) => todo._id !== id);
+    await axios.delete("http://localhost:5000/api/removeTodoById/" + id);
     setTodos(newTodos);
   }
 
@@ -35,7 +52,7 @@ const App = () => {
         <label>Title</label>
         <input
           type="text"
-          value={Task}
+          value={title}
           onChange={(event) => {
             setTask(event.target.value);
           }}
@@ -43,7 +60,7 @@ const App = () => {
         <label>Description</label>
         <input
           type="text"
-          value={Detail}
+          value={description}
           onChange={(event) => {
             setDetail(event.target.value);
           }}
@@ -55,10 +72,10 @@ const App = () => {
       <ul>
         {todos.map((todo, index) => {
           return (
-            <li key={todo.id}>
-              <p>{todo.Task}</p>
-              <p>{todo.Detail}</p>
-              <button type="button" onClick={() => deleteTodo(todo.id)}>
+            <li key={todo._id}>
+              <p>{todo.title}</p>
+              <p>{todo.description}</p>
+              <button type="button" onClick={() => deleteTodo(todo._id)}>
                 Delete
               </button>
             </li>
